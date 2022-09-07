567d7268d766877f15553ff06d7f49267632ff06
Innokentiy Borisov <ino.luckysurfer@gmail.com>
2022-09-07 23:12:40 +0400

    3.3 решена и проверена автотестами

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  str = str.split('-');
+  str.forEach((item, index) => {
+    if (index) {
+      str[index] = str[index].at(0).toUpperCase() + str[index].slice(1);
+    }
+  });
+  
+  return str.join('');
 }
