b2491ea835f764624bc473173b5a22f89f4f9384
Innokentiy Borisov <ino.luckysurfer@gmail.com>
2022-09-07 23:26:35 +0400

    3.4 решена и проверена автотестами

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
-  // ваш код...
+  users = users.filter(item => item.age <= age);
+  
+  let result = '';
+  for (let user of users) {
+    result += ((result && '\n') + `${user.name}, ${user.balance}`);
+  }
+  
+  return result;
 }
